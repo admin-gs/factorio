9742599a1957c92a7207632e6f04e25ddbdaf939
Thomas Biesaart <thomas@infi.nl>
2020-10-06 18:02:17 +0200

    Always override settings on boot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "/config/world.zip" ]; then
-  echo "This is the first run, generating map...."
-  mkdir -p /config
+mkdir -p /config
   jq \
     --arg serverName "$FACTORIO_SERVER_NAME" \
     --arg serverDescription "$FACTORIO_SERVER_DESCRIPTION" \
@@ -18,6 +16,8 @@ if [ ! -f "/config/world.zip" ]; then
      }' \
      /app/data/server-settings.example.json > /config/server-settings.json
 
+if [ ! -f "/config/world.zip" ]; then
+  echo "This is the first run, generating map...."
   factorio --create "/config/world";
 else
   echo "A map has already been generated!"
